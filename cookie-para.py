617e54d32a531f2836cd65af1d5a0755330af067
princewilling <princewillingoo@gmail.com>
2023-05-05 12:46:18 +0100

    Cookie Parameters¶

---
+++ cookie-para.py
@@ -0,0 +1,14 @@
+# Cookie ParametersÂ¶
+
+# You can define Cookie parameters the same way you define Query and Path parameters.
+
+from typing import Annotated
+
+from fastapi import Cookie, FastAPI
+
+app = FastAPI()
+
+
+@app.get("/items/")
+async def read_items(ads_id: Annotated[str | None, Cookie()] = None):
+    return {"ads_id": ads_id}
